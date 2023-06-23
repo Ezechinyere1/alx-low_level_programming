41cd2ed7ed3f520d9e54ad3c44da208eb455e817
Ezechinyere1 <philjames101010@gmail.com>
2023-06-23 09:42:45 -0700

    ALX

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
+
 /**
-* _isupper - checks for uppercase character
-* @c: Variable text
-* Return: Always 0.
-*/
+ * _isupper - checks for uppercase character
+ * @c: Variable text
+ * Return: Always 0.
+ */
 int _isupper(int c)
 {
-
 
 	if (c >= 'A' && c <= 'Z')
 	{
