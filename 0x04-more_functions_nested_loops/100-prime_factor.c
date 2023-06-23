215d8f4b9a08fb82f7c16467baaada13ff4e9376
Ezechinyere1 <philjames101010@gmail.com>
2023-06-23 11:41:52 -0700

    ALX

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,4 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include <math.h>
 
 /**
@@ -12,7 +12,7 @@ int main(void)
 	long int max;
 	long int i;
 
-	n = 612852475143;
+		n = 612852475143;
 	max = -1;
 
 	while (n % 2 == 0)
