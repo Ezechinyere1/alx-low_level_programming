798186889e327108e646a8dd4b7e1315edfd1179
Ezechinyere1 <philjames101010@gmail.com>
2023-06-23 09:53:02 -0700

    ALX

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
  * _isupper - checks for uppercase character
  * @c: Variable text
  * Return: Always 0.
@@ -10,20 +9,12 @@ int _isupper(int c)
 {
 
 	if (c >= 'A' && c <= 'Z')
-=======
-* _isupper - checks for uppercase character
-* @c: Variable text
-* @x: The number to be checked
-*
-* Return: 1 for upper letter or 0 for any else
-*/
-int _isupper(int x)
-{
-	if (x >= 65 && x <= 90)
->>>>>>> 0079e61d4b0c90db4a23150e809f47c1e7c8d130
 	{
 		return (1);
 	}
-	
-                return (0);       
+	else
+	{
+		return (0);
+	}
+
 }
